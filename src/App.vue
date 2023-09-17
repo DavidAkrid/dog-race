6ea8fe1772d4b933e22d612e9ca60583602d935f
Dakrid <davidakrid@gmail.com>
2023-09-17 14:03:17 -0500

    Initial commit to contain all code

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,71 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </nav>
+  </nav> -->
+  <header>
+    <h1>The Dog Race</h1>
+  </header>
   <router-view/>
+  <footer>Made by David Akridge</footer>
 </template>
 
+<script>
+  export default {
+    name: 'App',
+  }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;900&display=swap');
+
+html {
+  font-family: 'Poppins', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #E0E0CE;
+  background-color: #222222;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
+header h1 {
   font-weight: bold;
-  color: #2c3e50;
+  font-size: 100px;
+  margin: 0px;
+  color: #F2AF29;
+  text-shadow: 10px 5px 5px black;
+  text-decoration:underline;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+footer {
+  margin: 10px;
+}
+
+.button, button {
+  background: darkred;
+  color: #E0E0CE;
+  padding: 10px 20px;
+  margin: 10px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-transform: uppercase;
+  font-size: 15px;
+  font-weight: bold;
+  box-shadow: 5px 5px 5px black;
+  border: 2px solid rgb(107, 0, 0);
+  text-shadow: -3px 0 black, 0 3px black, 3px 0 black, 0 -3px black;
+}
+
+.button:hover, button:hover {
+  background: rgb(179, 1, 1);
+}
+
+.button:active, button:active {
+  background: rgb(90, 0, 0);
+  color: white;
+}
+
+.division{
+    border: 1px solid #E0E0CE;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Dashboard from '../views/Dashboard.vue'
+import Login from '../views/Login.vue'
+import Home from '../views/Home.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/dashboard',
+    name: 'dashboard',
+    component: Dashboard
   }
 ]
 
